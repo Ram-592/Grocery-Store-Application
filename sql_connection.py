c2171f600264ab3c801b4e4559ffc7912e2d29bd
Ramprasad M <123531137+Ram-592@users.noreply.github.com>
2024-11-17 22:46:06 +0530

    sql_connection.py

--- sql_connection.py
+++ sql_connection.py
@@ -6,7 +6,7 @@ def get_sql_connection():
     print("Opening mysql connection")
     global __cnx
     if __cnx is None:
-        __cnx = mysql.connector.connect(user='root', password='ram@M2002',
+        __cnx = mysql.connector.connect(user='root', password='use your password',
                                       host='localhost',port='3307',
                                       database='grocery_store')
 
